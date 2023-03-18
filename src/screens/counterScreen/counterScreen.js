81db4ece88e39c211ba5ce25ae14f6d1468266fe
danmesfin <danielmsfn@gmail.com>
2023-03-18 15:33:53 +0300

    initial commit

---
+++ src/screens/counterScreen/counterScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from '../store/reducers/counter';
+
+const CounterScreen = () => {
+  const counter = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <View>
+      <Text>{counter}</Text>
+      <Button title="Increment" onPress={() => dispatch(increment())} />
+      <Button title="Decrement" onPress={() => dispatch(decrement())} />
+    </View>
+  );
+};
+
+export default CounterScreen;
