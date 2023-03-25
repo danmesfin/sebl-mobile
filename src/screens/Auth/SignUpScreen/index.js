2ae85d8518c6cb98dcecf437bfe53da8cf921dc2
danmesfin <danielmsfn@gmail.com>
2023-03-25 23:29:14 +0300

    added loading indicator

--- src/screens/Auth/SignUpScreen/index.js
+++ src/screens/Auth/SignUpScreen/index.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, Button} from 'react-native';
-import {useDispatch} from 'react-redux';
+import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 import {signUpUser} from '../../../redux/authSlice';
+import Colors from '../../../styles/theme';
 
 const SignUpScreen = ({navigation}) => {
+  const {isLoading} = useSelector(state => state.auth);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -18,23 +20,56 @@ const SignUpScreen = ({navigation}) => {
   };
 
   return (
-    <View>
-      <Text>Sign Up Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign Up Screen</Text>
       <TextInput
+        style={styles.input}
         placeholder="Email"
         value={email}
         onChangeText={value => setEmail(value)}
       />
       <TextInput
+        style={styles.input}
         placeholder="Password"
         value={password}
         onChangeText={value => setPassword(value)}
         secureTextEntry={true}
       />
-      <Button title="Sign Up" onPress={handleSignUp} />
-      <Button title="Sign In" onPress={handleSignInNavigation} />
+      <Button
+        title="Sign Up"
+        onPress={handleSignUp}
+        disabled={isLoading}
+        color={Colors.accent}
+      />
+      <Button
+        title="Sign In"
+        onPress={handleSignInNavigation}
+        color={Colors.accent}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+    width: '100%',
+    borderRadius: 5,
+  },
+});
 
 export default SignUpScreen;
