fc5529fe30406d1b12e740503b86bb0f61b9de12
danmesfin <danielmsfn@gmail.com>
2023-03-28 22:26:48 +0300

    fixed Navigator Icons Issue

--- src/Navigations/HomeNavigation.js
+++ src/Navigations/HomeNavigation.js
@@ -4,53 +4,44 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/Home/HomeScreen';
 import CommunityScreen from '../screens/Community/CommunityScreen';
 import ProfileScreen from '../screens/Profile/ProfileScreen';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import {primaryColor, secondaryColor} from '../styles/theme';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Color from '../styles/theme';
+import {NavigationContainer} from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
 
 const HomeNavigator = () => {
   return (
     <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Community') {
+            iconName = 'users';
+          } else if (route.name === 'Profile') {
+            iconName = 'user';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
       tabBarOptions={{
-        activeTintColor: primaryColor,
-        inactiveTintColor: secondaryColor,
+        activeTintColor: Color.primary,
+        inactiveTintColor: Color.secondaryLight,
       }}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={
-          ({
-            tabBarIcon: ({color}) => (
-              <FontAwesome name="home" type="font-awesome" color={color} />
-            ),
-          },
-          {headerShown: false})
-        }
+        options={{headerTitle: 'Sebl'}}
       />
-      <Tab.Screen
-        name="Community"
-        component={CommunityScreen}
-        options={
-          ({
-            tabBarIcon: ({color}) => (
-              <FontAwesome name="users" type="font-awesome" color={color} />
-            ),
-          },
-          {headerShown: false})
-        }
-      />
+      <Tab.Screen name="Community" component={CommunityScreen} />
       <Tab.Screen
         name="Profile"
         component={ProfileScreen}
-        options={
-          ({
-            tabBarIcon: ({color}) => (
-              <FontAwesome name="user" type="font-awesome" color={color} />
-            ),
-          },
-          {headerShown: false})
-        }
+        options={{headerShown: false}}
       />
     </Tab.Navigator>
   );
---
+++ src/components/Weather/index.js
@@ -0,0 +1,70 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import axios from 'axios';
+
+const WeatherSection = () => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m',
+      )
+      .then(response => {
+        setWeatherData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!weatherData) {
+    return null;
+  }
+
+  const currentWeather = weatherData.current_weather;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Today's Weather</Text>
+      <View style={styles.weatherInfo}>
+        <Text style={styles.temperature}>{currentWeather.temperature}°C</Text>
+        <Text style={styles.weatherDescription}>
+          Wind: {currentWeather.windspeed} km/h
+        </Text>
+        <Text style={styles.weatherDescription}>
+          Humidity: {weatherData.hourly.relativehumidity_2m[0]}%
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 20,
+    marginVertical: 10,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  weatherInfo: {
+    alignItems: 'center',
+  },
+  temperature: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  weatherDescription: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 5,
+  },
+});
+
+export default WeatherSection;
