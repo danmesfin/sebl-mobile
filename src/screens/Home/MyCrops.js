d03355c2972bf455e8d3a79ac07328f972bdaeea
danmesfin <danielmsfn@gmail.com>
2023-04-04 23:53:32 +0300

    added shadow props

--- src/screens/Home/MyCrops.js
+++ src/screens/Home/MyCrops.js
@@ -35,24 +35,24 @@ const MyCrops = () => {
             />
           </TouchableOpacity>
         </View>
-        {/*Add two cards here cultivation tips and pest control */}
-        <View style={styles.container}>
-          <View style={styles.cardRow}>
-            <TouchableOpacity style={styles.card}>
-              <Image
-                source={require('../../../assets/icons/cultivation-tips.png')}
-                style={styles.cardImage}
-              />
-              <Text style={styles.cardTitle}>Cultivation Tips</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.card}>
-              <Image
-                source={require('../../../assets/icons/pest-control.png')}
-                style={styles.cardImage}
-              />
-              <Text style={styles.cardTitle}>Pest Control</Text>
-            </TouchableOpacity>
-          </View>
+      </View>
+      {/*Add two cards here cultivation tips and pest control */}
+      <View style={styles.container}>
+        <View style={styles.cardRow}>
+          <TouchableOpacity style={[styles.card, styles.shadowProp]}>
+            <Image
+              source={require('../../../assets/icons/cultivation-tips.png')}
+              style={styles.cardImage}
+            />
+            <Text style={styles.cardTitle}>Cultivation Tips</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
+            <Image
+              source={require('../../../assets/icons/pest-control.png')}
+              style={styles.cardImage}
+            />
+            <Text style={styles.cardTitle}>Pest Control</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -62,7 +62,7 @@ const MyCrops = () => {
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    backgroundColor: theme.primary,
+    // backgroundColor: theme.primary,
   },
   section: {
     backgroundColor: theme.secondaryDark,
@@ -114,16 +114,20 @@ const styles = StyleSheet.create({
   },
   cardRow: {
     marginTop: 16,
+    marginBottom: 8,
     flexDirection: 'row',
     justifyContent: 'center',
   },
   card: {
     backgroundColor: theme.secondary,
+    marginHorizontal: 4,
     width: '40%',
     aspectRatio: 1.5,
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
+    borderWidth: 1,
+    borderColor: theme.PrimaryBorder,
   },
   cardImage: {
     width: '60%',
@@ -135,6 +139,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: theme.textPrimary,
     fontSize: 16,
+  },
+  shadowProp: {
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
 });
 
