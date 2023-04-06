d844d3fd06dff2d8885a0f81633d2efd47aecd9d
danmesfin <danielmsfn@gmail.com>
2023-04-06 16:56:02 +0300

    reanimated issue - cmake

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import AuthNavigator from './Navigations/AuthNavigation';
 import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeNavigator from './Navigations/HomeNavigation';
 import CultivationTipsScreen from './screens/CultivationTipsScreen';
@@ -11,6 +12,7 @@ import PlantDiseaseNavigation from './Navigations/PlantDiseaseNavigation';
 import CustomSplashScreen from './screens/SplashScreen/SplashScreen';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const MainApp = () => {
   const [appReady, setAppReady] = useState(false);
@@ -40,15 +42,23 @@ const MainApp = () => {
         /> */}
         {user ? (
           <>
-            <Stack.Screen name="HomeNavigator" component={HomeNavigator} />
-            <Stack.Screen
-              name="PlantDiseaseNavigator"
-              component={PlantDiseaseNavigation}
-            />
-            <Stack.Screen
-              name="Cultivation Tips"
-              component={CultivationTipsScreen}
-            />
+            <Drawer.Navigator initialRouteName="Home">
+              <Drawer.Screen name="Home" component={HomeNavigator} />
+              <Drawer.Screen
+                name="PlantDiseaseNavigator"
+                component={PlantDiseaseNavigation}
+              />
+
+              <Stack.Screen name="HomeNavigator" component={HomeNavigator} />
+              <Stack.Screen
+                name="PlantDiseaseNavigator"
+                component={PlantDiseaseNavigation}
+              />
+              <Stack.Screen
+                name="Cultivation Tips"
+                component={CultivationTipsScreen}
+              />
+            </Drawer.Navigator>
           </>
         ) : (
           <>
