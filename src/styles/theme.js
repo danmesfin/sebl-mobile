597e368ef04eb26aa4ab34445d2f0f6aba578fe5
danmesfin <danielmsfn@gmail.com>
2023-06-01 16:47:49 +0300

    updated accent

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,7 +2,7 @@ export default {
   primary: '#32A753', // Green shade representing nature and freshness
   primaryLight: '#67CC83', // Lighter green shade for highlighting
   primaryDark: '#1E7C3E', // Darker green shade for contrast
-  accent: '#FFB900', // Accent color, such as for buttons or important elements
+  accent: '#67CC83', // Accent color, such as for buttons or important elements
   secondary: '#F8F8F8', // Light gray shade for secondary elements or backgrounds
   secondaryDark: '#EDEDED', // Darker gray shade for contrast
   text: '#333333', // Dark gray for main text
